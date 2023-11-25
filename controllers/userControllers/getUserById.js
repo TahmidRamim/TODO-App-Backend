48b28de1794b262add1760d87561df12093cf344
Ramim <thefactualramim@gmail.com>
2023-11-26 00:24:07 +0600

    fixed error

--- controllers/userControllers/getUserById.js
+++ controllers/userControllers/getUserById.js
@@ -5,7 +5,7 @@ export const getUserById = async (req, res, next) => {
     const { id } = req.params;
 
     const user = await User.findById(id);
-    console.log(user);
+   
     if (!user) {
       next(new CustomError("User not found", 404));
     }
