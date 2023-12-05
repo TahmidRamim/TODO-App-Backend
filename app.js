09be4d390bf14182433daa777a034fa3b118410e
Tahmid R <149571156+TahmidRamim@users.noreply.github.com>
2023-12-05 20:44:43 +0600

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
